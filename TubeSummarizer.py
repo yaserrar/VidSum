fa09206fbe6a5e0fab55c0f4541eae1490a79e25
Youssef Aserrar <youssefasrar2966@gmail.com>
2023-05-28 19:33:32 +0100

    first code

---
+++ TubeSummarizer.py
@@ -0,0 +1,87 @@
+
+from youtube_transcript_api import YouTubeTranscriptApi
+from pytube import extract, YouTube
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chat_models import ChatOpenAI
+import os
+from dotenv import load_dotenv
+from langchain.chains.summarize import load_summarize_chain
+from langchain.callbacks import get_openai_callback
+from langchain.vectorstores.faiss import FAISS
+from langchain.chains import RetrievalQA
+from langchain.embeddings import HuggingFaceHubEmbeddings
+import requests
+
+class TubeSummarizer:
+    
+    def __init__(self) :
+        load_dotenv()
+       
+    def is_valid_youtube_link(self, video_link):
+        try:
+            request = requests.get(video_link, allow_redirects=False)
+        except:
+            return False
+        return request.status_code == 200
+    
+    
+    def load_video_subtitles(self, video_link):
+        id = extract.video_id(video_link)
+        transcript = YouTubeTranscriptApi.get_transcript(id, languages=('en','fr'))
+        
+        video_script = ""
+
+        for text in transcript:
+            t = text["text"]
+            if t != '[Music]':
+                video_script += t + " "
+            
+        self.video_script = video_script   
+         
+        return self.video_script 
+    
+    
+    def summarize_video(self):
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=2048, chunk_overlap=50)
+        self.summarization_docs = text_splitter.create_documents([self.video_script])
+        
+        llm = ChatOpenAI(
+            temperature=0,
+            model_name="gpt-3.5-turbo",
+            openai_organization=os.environ.get("OPENAI_API_ORGANIZATION"),
+            openai_api_key=os.environ.get("OPENAI_API_KEY")
+            )
+        
+        chain = load_summarize_chain(llm=llm, chain_type="map_reduce")
+        
+        with get_openai_callback() as cb:
+            self.video_summarization = chain.run(self.summarization_docs)
+            print("Price: ", cb)
+            
+        return self.video_summarization
+                
+                
+    def embed_video_script(self):
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=50)
+        self.chat_docs = text_splitter.create_documents([self.video_script])
+        
+        embedding_model = HuggingFaceHubEmbeddings(
+                                                    repo_id="sentence-transformers/distiluse-base-multilingual-cased-v2", 
+                                                    huggingfacehub_api_token=os.environ.get("HUGGINGFACEHUB_API_TOKEN")
+                                                   )
+        
+        vector_store = FAISS.from_documents(self.chat_docs, embedding_model)
+        
+        llm = ChatOpenAI(
+            temperature=0,
+            model_name="gpt-3.5-turbo",
+            openai_organization=os.environ.get("OPENAI_API_ORGANIZATION"),
+            openai_api_key=os.environ.get("OPENAI_API_KEY")
+            )
+        
+        self.chain = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=vector_store.as_retriever())
+        
+        
+    def chat_video(self, question):
+        answer = self.chain.run(question)  
+        return answer
---
+++ app.py
@@ -0,0 +1,93 @@
+import streamlit as st
+from TubeSummarizer import TubeSummarizer
+import streamlit_scrollable_textbox as stx
+from streamlit_chat import message
+
+st.set_page_config(page_title="TubeSummarizer") # page_icon=":smiley:", 
+
+if "questions" not in st.session_state:
+    st.session_state.questions = []
+    
+if "answers" not in st.session_state:
+    st.session_state.answers = []
+    
+@st.cache_resource
+def initailize_summarizer(video_link):
+    tube_summarizer = TubeSummarizer()
+    st.session_state.questions = []
+    st.session_state.answers = []
+    return tube_summarizer
+    
+@st.cache_resource
+def load_subtitles(video_link):
+    subtitles = tube_summarizer.load_video_subtitles(video_link=video_link)
+    return subtitles
+
+@st.cache_resource
+def generate_summary(video_link):
+    summary = tube_summarizer.summarize_video()
+    return summary
+
+
+@st.cache_resource
+def embed_script(video_link):
+    tube_summarizer.embed_video_script()
+     
+
+
+st.header("TubeSummarizer")
+st.markdown('#') 
+
+st.markdown('#### I : YouTube Video URL')
+video_link = st.text_input("URL :", placeholder="https://www.youtube.com/watch?v=************")
+
+tube_summarizer = initailize_summarizer(video_link)
+
+if tube_summarizer.is_valid_youtube_link(video_link):
+    st.video(video_link)
+
+    # with st.spinner("Extracting subtitles..."):
+    subtitles = load_subtitles(video_link)
+
+    if subtitles and len(subtitles) <= 20000:
+
+        st.markdown('#### II. Subtitles')
+        stx.scrollableTextbox(subtitles, height=200)
+
+        st.markdown('#### III. Summarization / Q&A')
+        choice = st.selectbox("Please choose an option :", ('Summarization', 'Q&A'))
+
+        if choice == "Summarization":
+            if st.button("Generate Summary"):
+
+                # with st.spinner("Generating summary..."):
+                summary = generate_summary(subtitles)
+
+                stx.scrollableTextbox(summary, height=200)
+        else:
+            question = st.text_input("Question: ", placeholder="Type your question here")
+            if st.button("Generate Answer"):
+                embed_script(subtitles)
+                st.session_state.questions.append(question)
+                
+                with st.spinner("Answering..."):
+                    answer = tube_summarizer.chat_video(question)
+                    
+                st.session_state.answers.append(answer)
+                
+            if st.button("Clear chat"):
+                st.session_state.questions = []
+                st.session_state.answers = []
+            
+            
+            for i in reversed(list(range(len(st.session_state.questions)))) :
+                try:
+                    message(st.session_state.answers[i]) 
+                    message(st.session_state.questions[i], is_user=True)  # align's the message to the right
+                except:
+                    pass
+    else:
+        st.error("Your video is too long, It's gonna cost too much :)")
+else:
+    if video_link != "":
+        st.error("Please provide a valid YouTube video URL")
